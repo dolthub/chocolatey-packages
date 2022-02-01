b49cfdefd11ada109871f2b5da1ae758cffe6864
Dustin Brown <dustin@dolthub.com>
2022-01-31 16:54:20 -0800

    dolt/tools/chocolateyInstall.ps1: clear generated fields

--- dolt/tools/chocolateyInstall.ps1
+++ dolt/tools/chocolateyInstall.ps1
@@ -5,9 +5,9 @@ $packageArgs = @{
   fileType       = 'MSI'
   softwareName   = 'dolt'
 
-  checksum       = '80e7401dee4977289c8d26e1c1e0717ba60555056299b3bf1671d0ec784df315'
-  checksumType   = 'sha256'
-  url            = 'https://github.com/dolthub/dolt/releases/download/v0.34.7/dolt-windows-amd64.msi'
+  checksum       = ''
+  checksumType   = ''
+  url            = ''
 
   silentArgs     = '/quiet'
   validExitCodes = @(0)
