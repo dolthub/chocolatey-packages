02cfda9c2af7bf43ecbb6cdc790d697c5c154d8e
coffeegoddd <coffeegoddd@users.noreply.github.com>
2022-08-04 18:24:04 +0000

    [ga-bump-dolt-chocolatey] Bumping Dolt on Chocolatey 0.40.22

--- dolt/tools/chocolateyInstall.ps1
+++ dolt/tools/chocolateyInstall.ps1
@@ -5,9 +5,9 @@ $packageArgs = @{
   fileType       = 'MSI'
   softwareName   = 'dolt'
 
-  checksum       = '6283f02d7b4e55c44528b807f92ea5b24703da9c3a0714f3c571e1c8f4fa2546'
+  checksum       = '4ca35d8b6af5904020077a13c87f88e44d33548618d4a9baa73a131fb133ad1e'
   checksumType   = 'sha256'
-  url            = 'https://github.com/dolthub/dolt/releases/download/v0.40.21/dolt-windows-amd64.msi'
+  url            = 'https://github.com/dolthub/dolt/releases/download/v0.40.22/dolt-windows-amd64.msi'
 
   silentArgs     = '/quiet'
   validExitCodes = @(0)
